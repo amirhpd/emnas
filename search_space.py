3908375657314ac74b88f3338478ec3802d4a5cd
amirhossein <ahpd1068@yahoo.com>
2021-01-16 19:01:13 +0100

    search_space done, tokens as str

--- search_space.py
+++ search_space.py
@@ -1,89 +1,107 @@
 """
 search_space
-creates different possibilities of NN in form of mapping
-converts a mapping to keras model
+creates different possibilities of NN as a dict of tokens and layer info
+translates a sequence of tokens to layer info
+converts a token sequence to keras model
 """
-import os
-import warnings
-from typing import Dict, List, Tuple
-import pandas as pd
 from keras import optimizers
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Dropout
+import keras
+import itertools
 
 
 class SearchSpace(object):
 
-    def __init__(self, target_classes):
+    def __init__(self, model_output_shape):
 
-        self.target_classes = target_classes
-        self.mapping = self.create_mapping()
-        self.architecture_dropout = 0.2
-        self.architecture_loss_function = "categorical_crossentropy"
-        self.architecture_optimizer = "Adam"
-        self.architecture_lr = 0.01
-        self.architecture_decay = 0.0
+        self.model_output_shape = model_output_shape
+        self.model_dropout = 0.2
+        self.model_loss_function = "categorical_crossentropy"
+        self.model_optimizer = "Adam"
+        self.model_lr = 0.01
+        self.model_decay = 0.0
         self.metrics = ["accuracy"]
 
-    def create_mapping_cnn(self):
-        pass
+    def generate_token(self):
+        nodes = [8, 16, 32, 64, 128, 256]
+        layers = ["Conv2D", "DepthwiseConv2D"]
+        filters = [8, 16, 24, 32, 40, 48]
+        kernel_sizes = [(1, 1), (2, 2), (3, 3)]
+        strides = [(1, 1), (2, 2), (3, 3)]
+        paddings = ["valid", "same"]
+        activations = ["sigmoid", "tanh", "relu", "elu"]
 
-    def create_mapping(self) -> Dict[int, Tuple[int, str]]:
-        nodes = [8, 16, 32, 64, 128, 256, 512]
-        act_funcs = ['sigmoid', 'tanh', 'relu', 'elu']
-        layer_params = []
-        layer_id = []
-        for i in range(len(nodes)):
-            for j in range(len(act_funcs)):
-                layer_params.append((nodes[i], act_funcs[j]))
-                layer_id.append(len(act_funcs) * i + j + 1)
-        vocab = dict(zip(layer_id, layer_params))
-        vocab[len(vocab) + 1] = (('dropout'))
-        if self.target_classes == 2:
-            vocab[len(vocab) + 1] = (self.target_classes - 1, 'sigmoid')
+        # keys as int
+        # cnn_params = list(itertools.product(*[layers, filters, kernel_sizes, strides, paddings, activations]))
+        # dense_params = list(itertools.product(*[["Dense"], nodes, activations]))
+        # params = cnn_params + dense_params
+        # count = range(1, len(params)+1)
+        # token = dict(zip(count, params))
+        #
+        # token[count[-1]+1] = "dropout"
+        # if self.model_output_shape == 1:
+        #     token[count[-1]+2] = (1, "sigmoid")
+        # else:
+        #     token[count[-1]+2] = (self.model_output_shape, "softmax")
+
+        # keys as str
+        cnn_params = list(itertools.product(*[layers, filters, kernel_sizes, strides, paddings, activations]))
+        cnn_count = range(1, len(cnn_params)+1)
+        cnn_keys = [f"c{i}" for i in cnn_count]
+        cnn_token = dict(zip(cnn_keys, cnn_params))
+
+        dense_params = list(itertools.product(*[["Dense"], nodes, activations]))
+        dense_count = range(1, len(dense_params)+1)
+        dense_keys = [f"d{i}" for i in dense_count]
+        dense_token = dict(zip(dense_keys, dense_params))
+
+        dense_token["drp"] = "dropout"
+        if self.model_output_shape == 1:
+            dense_token["out"] = (1, "sigmoid")
         else:
-            vocab[len(vocab) + 1] = (self.target_classes, 'softmax')
-        return vocab
+            dense_token["out"] = (self.model_output_shape, "softmax")
 
-    def encode_sequence(self, sequence: List[Tuple[int, str]]) -> List[int]:
-        keys = list(self.mapping.keys())
-        values = list(self.mapping.values())
-        encoded_sequence = []
-        for value in sequence:
-            encoded_sequence.append(keys[values.index(value)])
-        return encoded_sequence
+        return {**cnn_token, **dense_token}
 
-    def decode_sequence(self, sequence: Dict[int, Tuple[int, str]]) -> List[Tuple[int, str]]:
-        keys = list(self.mapping.keys())
-        values = list(self.mapping.values())
-        decoded_sequence = []
-        for key in sequence:
-            decoded_sequence.append(values[keys.index(key)])
-        return decoded_sequence
+    def translate_sequence(self, sequence):
+        token = self.generate_token()
+        translated = []
+        for layer in sequence:
+            layer_info = token[layer]
+            translated.append(layer_info)
+        return translated
 
-    def create_architecture(self, sequence: List[int], mlp_input_shape: Tuple):
-        layer_configs = self.decode_sequence(sequence)
+    def create_model(self, sequence, model_input_shape):
+        layers_info = self.translate_sequence(sequence)
         model = Sequential()
-        if len(mlp_input_shape) > 1:
-            model.add(Flatten(name='flatten', input_shape=mlp_input_shape))
-            for i, layer_conf in enumerate(layer_configs):
-                if layer_conf is 'dropout':
-                    model.add(Dropout(self.architecture_dropout, name='dropout'))
-                else:
-                    model.add(Dense(units=layer_conf[0], activation=layer_conf[1]))
+        if layers_info[0][0] == "Conv2D":
+            model.add(keras.layers.Conv2D(filters=layers_info[0][1], kernel_size=layers_info[0][2],
+                                          strides=layers_info[0][3], padding=layers_info[0][4],
+                                          activation=layers_info[0][5], input_shape=model_input_shape))
+        elif layers_info[0][0] == "DepthwiseConv2D":
+            model.add(keras.layers.DepthwiseConv2D(kernel_size=layers_info[0][2], strides=layers_info[0][3],
+                                                   padding=layers_info[0][4], activation=layers_info[0][5],
+                                                   input_shape=model_input_shape))
         else:
-            for i, layer_conf in enumerate(layer_configs):
-                if i == 0:
-                    model.add(Dense(units=layer_conf[0], activation=layer_conf[1], input_shape=mlp_input_shape))
-                elif layer_conf is 'dropout':
-                    model.add(Dropout(self.architecture_dropout, name='dropout'))
-                else:
-                    model.add(Dense(units=layer_conf[0], activation=layer_conf[1]))
+            raise ValueError("Layer Type Unknown")
 
-        if self.target_classes == 2:
-            self.architecture_loss_function = "binary_crossentropy"
-        optimizer = getattr(optimizers, self.architecture_optimizer)(lr=self.architecture_lr,
-                                                                     decay=self.architecture_decay)
-        model.compile(loss=self.architecture_loss_function, optimizer=optimizer, metrics=self.metrics)
+        for layer in layers_info[1:-1]:
+            if layer[0] == "Conv2D":
+                model.add(keras.layers.Conv2D(filters=layer[1], kernel_size=layer[2], strides=layer[3],
+                                              padding=layer[4], activation=layer[5]))
+            elif layer[0] == "DepthwiseConv2D":
+                model.add(keras.layers.DepthwiseConv2D(kernel_size=layer[2], strides=layer[3], padding=layer[4],
+                                                       activation=layer[5]))
+            elif layer[0] == "Dense":
+                model.add(keras.layers.Dense(units=layer[1], activation=layer[2]))
+            else:
+                raise ValueError("Layer Type Unknown")
+
+        model.add(keras.layers.Dense(units=layers_info[-1][0], activation=layers_info[-1][1]))
+
+        if self.model_output_shape == 1:
+            self.model_loss_function = "binary_crossentropy"
+        optimizer = getattr(optimizers, self.model_optimizer)(lr=self.model_lr, decay=self.model_decay)
+        model.compile(loss=self.model_loss_function, optimizer=optimizer, metrics=self.metrics)
 
         return model
--- tests.py
+++ tests.py
@@ -1,19 +1,15 @@
-import numpy as np
 import keras
-
 from search_space import SearchSpace
 
 
 def test_search_space():
-    search_space = SearchSpace(target_classes=2)
-    vocab = search_space.mapping
-    vocab_decoded = search_space.decode_sequence(vocab)
-    vocab_encoded = search_space.encode_sequence(vocab_decoded)
+    search_space = SearchSpace(model_output_shape=1)
+    tokens = search_space.generate_token()
 
-    sample_sequence = [8, 8, 30]
-    input_shape = np.shape(np.zeros(13))
-    model = search_space.create_architecture(sample_sequence, input_shape)
+    sample_sequence = ["c8", "c52", "d14", "out"]
+    translated_sequence = search_space.translate_sequence(sample_sequence)
+
+    model = search_space.create_model(sample_sequence, (128, 128, 3))
     keras.utils.plot_model(model, to_file="model.png", show_shapes=True)
-    assert len(vocab) == 30
-    assert len(vocab_decoded) == 30
-    assert len(vocab_encoded) == 30
+    print(model.summary())
+    assert len(tokens) == 890
